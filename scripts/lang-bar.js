1c6f90862c9be8d89993d1533ff10f1f23539acc
LightNeverDies <LightNeverDies@abv.bg>
2019-06-28 23:38:18 +0300

    Co-authored-by: Александър Господинов <nightavenger54@abv.bg>

---
+++ scripts/lang-bar.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    $(".lang-bar").click(function(){
+        if(!$(this).hasClass('open')){
+            $(this).attr('is-selector-active', 'true');
+            $(this).addClass('open');
+        }else{
+            $(this).attr('is-selector-active', 'false');
+            $(this).removeClass('open');
+        }
+    }) 
+});
