cace96cc1557050c679194ffe1532d9322999fa3
LightNeverDies <LightNeverDies@abv.bg>
2019-07-02 17:58:11 +0300

    Co-authored-by: Александър Господинов <nightavenger54@abv.bg>

--- index.php
+++ index.php
@@ -1,3 +1,49 @@
-<?php
-    echo "<p>Test</p>";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Resonance Services</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="styles/scss/main.css"/>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script>window.jQuery || document.write('<script src="/libs/jquery-3.4.1.min.js"><\/script>');</script>
+    </head>
+    <body>
+       <div class="main-wrapper">
+            <header>
+                <div class="header-top">
+                    <img class="logo" src="images/logo/pandalogo.png" >
+                    <div class="header-top-mid"></div>
+                    <div class="lang-bar-wrapper">
+                        <div class="lang-bar" is-selector-active="false">
+                            <span class="lang-name" title="Bulgaria - Български" target-lang="BG"></span>
+                            <div class="lang-dropdown-wrapper"> 
+                                <ul class="lang-dropdown-list">
+                                    
+                               </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="header-bottom">
+                    <nav class="nav-bar-wrapper" id="side_menu" is-selector-active="false">
+                            <input type="checkbox" id="toggle"/>
+                            <label for="toggle" class="hamb">&#9776;</label> 
+                        <ul class="nav-bar">
+                            <li class="nav-btn home-btn"><a href="#" class="nav-btn-txt home-btn-txt">Начало</a></li>
+                            <li class="nav-btn hosting-btn"><a href="#" class="nav-btn-txt hosting-btn-txt">Хостинг</a></li>
+                            <li class="nav-btn about-btn"><a href="#" class="nav-btn-txt about-btn-txt">За нас</a></li>                     
+                        </ul>
+                    </nav>
+                </div>
+                <div></div>
+            </header>
+            <main>
+            </main>
+            <footer>
+
+            </footer>
+       </div>
+       <script src="scripts/lang-bar.js"></script>
+    </body>
+</html>
